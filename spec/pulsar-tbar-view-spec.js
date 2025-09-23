aef66207c28a8dc21311e3c6660d1d0f2e76e323
Mark Simon <mark@manngo.net>
2025-09-23 18:02:47 +1000

    Initial commit

---
+++ spec/pulsar-tbar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PulsarTbarView from '../lib/pulsar-tbar-view';
+
+describe('PulsarTbarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
